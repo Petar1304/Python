3609a87584f1917efba7788d3e9b055584bdde45
Petar1304 <67274324+Petar1304@users.noreply.github.com>
2022-03-28 15:42:32 +0200

    Delete pdf_csv-main directory

--- pdf_csv-main/convert.py
+++
@@ -1,6 +0,0 @@
-import tabula
-
-file = '/home/petar/Documents/pdf_csv/smerovi.pdf'
-csv = '/home/petar/Documents/pdf_csv/smerovi.csv'
-
-tabula.convert_into(file, csv, output_format='csv', pages='all')
