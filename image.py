131333a7039cf6bc53e44b74c95d0c845260e689
Petar <67274324+Petar1304@users.noreply.github.com>
2022-05-30 19:16:07 +0200

    Add files via upload

---
+++ image.py
@@ -0,0 +1,35 @@
+import cv2
+'''
+# reading image
+img = cv2.imread('img.png')
+cv2.imshow('WINDOW NAME', img)
+'''
+# reading video
+cap = cv2.VideoCapture(0)
+
+while True:
+  ret, frame = cap.read()
+  
+  if not ret:
+    break
+
+  # resized_frame = cv2.resize(frame, (500, 500))
+
+  # hsv = cv2.cvtColor(resized_frame, cv2.COLOR_BGR2GRAY)
+
+  # cv2.rectangle(resized_frame, (50, 50), (350, 350) , (0, 0, 255), thickness=1)
+  
+  # drawing a line 
+  cv2.line(frame, (100, 100), (400, 400), (0, 255, 0), 5)
+
+  cv2.imshow('Web Cam', frame)
+
+  if cv2.waitKey(1) == ord('q'):
+    break
+
+
+print(f'Shape: {frame.shape}')
+
+cap.release()
+cv2.destroyAllWindows()
+
